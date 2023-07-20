410156ee125e48941fb0869e653898fe35b49705
r4mune <140058657+r4mune@users.noreply.github.com>
2023-07-20 17:47:56 +0900

    Add files via upload

---
+++ 自作サイト/script.js
@@ -0,0 +1,129 @@
+particlesJS("particles-js",{
+	"particles":{
+		"number":{
+			"value":38,//この数値を変更すると幾何学模様の数が増減できる
+			"density":{
+				"enable":true,
+				"value_area":800
+			}
+		},
+		"color":{
+			"value":"#ffffff"//色
+		},
+		"shape":{
+			"type":"polygon",//形状はpolygonを指定
+			"stroke":{
+				"width":0,
+			},
+	"polygon":{
+		"nb_sides":3//多角形の角の数
+	},
+	"image":{
+		"width":190,
+		"height":100
+	}
+	},
+		"opacity":{
+		"value":0.664994832269074,
+		"random":false,
+		"anim":{
+			"enable":true,
+			"speed":2.2722661797524872,
+			"opacity_min":0.08115236356258881,
+			"sync":false
+		}
+		},
+		"size":{
+			"value":3,
+			"random":true,
+			"anim":{
+				"enable":false,
+				"speed":40,
+				"size_min":0.1,
+				"sync":false
+			}
+		},
+		"line_linked":{
+			"enable":true,
+			"distance":150,
+			"color":"#ffffff",
+			"opacity":0.6,
+			"width":1
+		},
+		"move":{
+			"enable":true,
+			"speed":6,//この数値を小さくするとゆっくりな動きになる
+			"direction":"none",//方向指定なし
+			"random":false,//動きはランダムにしない
+			"straight":false,//動きをとどめない
+			"out_mode":"out",//画面の外に出るように描写
+			"bounce":false,//跳ね返りなし
+			"attract":{
+				"enable":false,
+				"rotateX":600,
+				"rotateY":961.4383117143238
+			}
+		}
+	},
+	"interactivity":{
+		"detect_on":"canvas",
+		"events":{
+			"onhover":{
+				"enable":false,
+				"mode":"repulse"
+			},
+	"onclick":{
+		"enable":false
+	},
+	"resize":true
+		}
+	},
+	"retina_detect":true
+});
+
+//テキストのカウントアップ+バーの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+	easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+	duration: 1000,//時間指定(1000＝1秒)
+	strokeWidth: 0.2,//進捗ゲージの太さ
+	color: '#555',//進捗ゲージのカラー
+	trailWidth: 0.2,//ゲージベースの線の太さ
+	trailColor: '#bbb',//ゲージベースの線のカラー
+	text: {//テキストの形状を直接指定				
+		style: {//天地中央に配置
+			position: 'absolute',
+			left: '50%',
+			top: '50%',
+			padding: '0',
+			margin: '-30px 0 0 0',//バーより上に配置
+			transform:'translate(-50%,-50%)',
+			'font-size':'1rem',
+			color: '#fff',
+		},
+		autoStyleContainer: false //自動付与のスタイルを切る
+	},
+	step: function(state, bar) {
+		bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+	}
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+	$("#splash_text").fadeOut(10);//フェイドアウトでローディングテキストを削除
+	$(".loader_cover-up").addClass("coveranime");//カバーが上に上がるクラス追加
+	$(".loader_cover-down").addClass("coveranime");//カバーが下に下がるクラス追加
+	$("#splash").fadeOut();//#splashエリアをフェードアウト
+});
+
+$('.slider').slick({
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    speed: 500,//スライドのスピード。初期値は300。
+    slidesToShow: 3,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    centerMode: true,//要素を中央ぞろえにする
+    variableWidth: true,//幅の違う画像の高さを揃えて表示
+    dots: true,//下部ドットナビゲーションの表示
+});
